664ff59e00f481cab671a6d4a73b6377246b613b
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-11-27 18:32:37 -0800

    Further work on the vmrun interface.

--- d.js
+++ d.js
@@ -6,6 +6,15 @@ vmrun.listMachines()
   var machine = machines[0];
   machine.user(user);
   return machine.start()
+  .then(function() {
+    return machine.script('echo foofoofoofoo')
+    .then(function(result) {
+      return result.stdout()
+      .then(function(stdout) {
+        console.log('STDOUT: ' + stdout);
+      });
+    });
+  })
   .finally(function() {
     return machine.stop();
   });
--- vmrun/vmrun.js
+++ vmrun/vmrun.js
@@ -5,12 +5,48 @@ var Promise = require('bluebird');
 var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
+var uuid = require('uuid');
+var os = require('os');
+var util = require('util');
+
+/*
+ * ############################################################################
+ * ############################################################################
+ * ############################################################################
+ */
 
 function User(u, p) {
   this.u = u;
   this.p = p;
 }
 
+/*
+ * ############################################################################
+ * ############################################################################
+ * ############################################################################
+ */
+
+function Result(config) {
+  this.machine = config.machine;
+  this.stdoutFile = config.stdout;
+  this.stderrFile = config.stderr;
+}
+
+Result.prototype.stdout = function() {
+  var self = this;
+  return self.machine.getFileRead(self.stdoutFile);
+};
+
+Result.prototype.stderr = function() {
+  var self = this;
+  return self.machine.getFileRead(self.stderrFile);
+};
+
+/*
+ * ############################################################################
+ * ############################################################################
+ * ############################################################################
+ */
 function Machine() {
   this.path = path.join(
     '~',
@@ -36,9 +72,14 @@ Machine.prototype.__exec = function(cmd) {
     ps.stdout.on('data', function(data) {
       console.log('OUT: %s', data);
     });
-    /*ps.stderr.on('data', function(data) {
-      console.log('ERR: %s', data);
-    });*/
+    ps.stderr.on('data', function(data) {
+      if (_.contains(data, 'JackRouter')) {
+      } else if (_.contains(data, 'SeratoVirtualAudio')) {
+        
+      } else {
+        console.log('ERR: %s', data);
+      }
+    });
     ps.on('exit', function(code, signal) {
       console.log('Exit: %s', code);
       if (code === 0) {
@@ -101,7 +142,7 @@ Machine.prototype.stop = function() {
 };
 
 Machine.prototype.waitDarwin = function(user) {
-  return this.__execVmrun('runScriptInGuest', {
+  return this.__execVmrun('runProgramInGuest', {
     user: user,
     args: ['/bin/echo', 'foo']
   });
@@ -116,6 +157,125 @@ Machine.prototype.wait = function(user) {
     default:
       throw new Error('OS not supported: ' + os);
   };
+};
+
+Machine.prototype.toGuestFile = function(fileIn) {
+  var self = this;
+  var fileOut = path.join(
+    '/',
+    'home',
+    self.user.u,
+    path.basename(fileIn)
+  );
+  console.log('File out: %s', fileOut);
+  return fileOut;
+};
+
+Machine.prototype.putFile = function(fileIn) {
+  var self = this;
+  var fileOut = self.toGuestFile(fileIn);
+  return self.__execVmrun('CopyFileFromHostToGuest', {
+    args: [
+      fileIn,
+      fileOut
+    ]
+  });
+};
+
+Machine.prototype.getFile = function(remoteFile, localDir) {
+  var self = this;
+  var localFile = path.join(localDir, path.basename(remoteFile));
+  return self.__execVmrun('CopyFileFromGuestToHost', {
+    args: [
+      remoteFile,
+      localFile
+    ]
+  })
+  .return(localFile);
+};
+
+/*
+ * Get file from vm, read file, remove file, return file contents.
+ */
+Machine.prototype.getFileRead = function(remoteFile) {
+  var self = this;
+  // Get local directory for the file.
+  var localDir = os.tmpdir();
+  // Get file from vm.
+  return self.getFile(remoteFile, os.tmpdir())
+  // Read file, remove file, and return file contents.
+  .then(function(localFile) {
+    // Read file.
+    return Promise.fromNode(function(cb) {
+      fs.readFile(localFile, cb);
+    })
+    // Remove file.
+    .tap(function() {
+      return Promise.fromNode(function(cb) {
+        fs.unlink(localFile, cb);
+      });
+    });
+  });
+};
+
+/*
+ * Run a script in the vm.
+ */
+Machine.prototype.script = function(s) {
+  // Save reference.
+  var self = this;
+  // Check to see if 's' is a file that exists.
+  return Promise.fromNode(function(cb) {
+    fs.exists(s, function(exists) {
+      cb(null, exists);
+    });
+  })
+  // If 's' is not a file that exists then treat it as a script string and
+  // write it to a temp file.
+  .then(function(exists) {
+    if (exists) {
+      // 's' is a file that exists.
+      return s;
+    } else {
+      // 's' is a script string, write it to a temp temp file.
+      var file = path.join(os.tmpdir(), uuid.v1() + '.sh');
+      return Promise.fromNode(function(cb) {
+        fs.writeFile(file, s, {mode: '0500'}, cb);
+      })
+      .delay(5 * 1000)
+      .return(file);
+    }
+  })
+  .then(function(file) {
+    // Put script into vm.
+    return self.putFile(file)
+    // Run the script in the vm.
+    .then(function() {
+      var id = uuid.v1();
+      var stdout = self.toGuestFile(id + '-stdout.log');
+      var stderr = self.toGuestFile(id + '-stderr.log');
+      var cmd = util.format(
+        '"%s 1> %s 2> %s"',
+        self.toGuestFile(file),
+        stdout,
+        stderr
+      );
+      return self.__execVmrun('runScriptInGuest', {
+        args: [
+          '/bin/bash',
+          cmd
+        ]
+      })
+      .then(function() {
+        return new Result({
+          machine: self,
+          stdout: stdout,
+          stderr: stderr
+        });
+      });
+    });
+  });
+
 };
 
 Machine.prototype.user = function(u, p) {
