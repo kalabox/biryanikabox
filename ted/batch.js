044e9e3e6f140bfbcb7a5f93655a55514fb8d0b8
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-12-02 10:41:18 -0800

    Made batch use events so we can stream results instead of waiting until all testing is done.

--- ted/batch.js
+++ ted/batch.js
@@ -1,16 +1,21 @@
 'use strict';
 
+var _ = require('lodash');
 var Promise = require('bluebird');
 var exec = require('child_process').exec;
 var VError = require('verror');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
 /*
  * Constructor: a batch is a group of tests run together.
  */
 function Batch(config) {
-  this.reporter = config.reporter || 'JSON';
+  this.reporter = config.reporter || 'json-stream';
   this.files = config.files;
+  EventEmitter.call(this);
 }
+util.inherits(Batch, EventEmitter);
 
 /*
  * Run batch through mocha and return json results.
@@ -18,7 +23,7 @@ function Batch(config) {
 Batch.prototype.run = function() {
   var self = this;
   // Execute mocha cli command.
-  return Promise.fromNode(function(cb) {
+  return Promise.try(function() {
     // Build command.
     var cmd = [
       'mocha',
@@ -28,22 +33,49 @@ Batch.prototype.run = function() {
     cmd = cmd.concat(self.files);
     cmd = cmd.join(' ');
     // Execute!
-    exec(cmd, function(err, stdout, stderr) {
-      // @todo: not sure if this is the best thing to do here.
-      if (err) {
-        console.log(stderr);
+    return exec(cmd);
+  })
+  // Handle events and end.
+  .then(function(ps) {
+    // This will later on be set by and end event and returned with promise.
+    var result = null;
+    // Handle lines of data coming back from child process.
+    ps.stdout.on('data', function(data) {
+      // Parse line data.
+      var parts = JSON.parse(data);
+      // Validate parsing of the data.
+      if (parts.length !== 2) {
+        throw new Error(util.format('Invalid event data: "%s"', data));
       }
-      cb(null, stdout);
+      // Build event object.
+      var key = parts[0];
+      var val = parts[1];
+      var evt = {};
+      evt[key] = val;
+      if (key === 'end') {
+        // Set result here so it can be returned with promise.
+        result = val;
+      }
+      // Emit progress event.
+      self.emit('progress', evt);
+    });
+    // Fulfill promise based on child process behavior.
+    return Promise.fromNode(function(cb) {
+      ps.on('error', cb);
+      ps.on('exit', function() {
+        // Emit exit event.
+        self.emit('end');
+        // Fulfill promise after call stack has completed.
+        process.nextTick(function() {
+          cb(null, result);
+        });
+      });
     });
   })
-  // Parse and return json results.
-  .then(function(data) {
-    return Promise.try(function() {
-      return JSON.parse(data);
-    })
-    .catch(function(err) {
-      throw new VError(err, 'Failed to parse JSON: %s', data);
-    });
+  // Emit error event.
+  .catch(function(err) {
+    self.emit('error', err);
+    throw err;
   });
 };
 
--- ted/exampleBatch.js
+++ ted/exampleBatch.js
@@ -1,7 +1,14 @@
 var Batch = require('./batch.js');
 
 var batch = new Batch({
-  files: ['./test1.js']
+  files: [
+    './test1.js',
+    './test2.js'
+  ]
+});
+
+batch.on('progress', function(evt) {
+  console.log('EVT: ' + JSON.stringify(evt, null, '  '));
 });
 
 batch.run()
