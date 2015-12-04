005de581efcfe1a043e684f979743d54be6a11b5
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-12-04 15:45:49 -0800

    Saving TED changes.

---
+++ ted/ted.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// @todo: comment and clean up.
+
+var _ = require('lodash');
+var argv = require('yargs').argv;
+var fs = require('fs');
+var Promise = require('bluebird');
+var http = require('http');
+var url = require('url');
+var yaml = require('./yaml.js');
+
+var config = {
+  hostname: 'localhost',
+  port: 8080
+};
+
+Promise.try(function() {
+  return Promise.fromNode(function(cb) {
+    fs.exists('./ted.yml', function(exists) {
+      if (exists) {
+        yaml.parseFile('./ted.yml')
+        .then(function(opts) {
+          _.extend(config, opts);
+        })
+        .nodeify(cb);
+      } else {
+        cb();
+      }
+    });
+  });
+})
+.then(function() {
+  console.log(config);
+})
+.then(function() {
+  return Promise.fromNode(function(cb) {
+    fs.readFile(argv.y, {encoding: 'utf8'}, cb);
+  })
+})
+.then(function(data) {
+
+  var postData = JSON.stringify({
+    yaml: data
+  });
+
+  var opts = {
+    hostname: config.hostname,
+    port: config.port,
+    path: '/test',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length
+    }
+  };
+
+  return Promise.fromNode(function(cb) {
+
+    var req = http.request(opts, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function(data) {
+        var json = JSON.parse(data);
+        console.log(JSON.stringify(json, null, '  '));
+      });
+      res.on('error', cb);
+      res.on('end', cb);
+    });
+
+    req.write(postData);
+    req.end();
+    
+  });
+
+});
--- ted/testInstall.js
+++ ted/testInstall.js
@@ -1,48 +1,27 @@
 
 ted.describe('kalabox', function(tag) {
 
-  var vm = ted.driver.vm(tag);
+  var vm = null;
+
+  before(function() {
+    vm = ted.driver.install(tag);
+    return vm.promise();
+  });
 
   after(function() {
-    return vm.done();
+    return vm.cleanup();
   });
 
-  it('should have a working echo', function() {
-    return vm.run('which echo')
-    .run('which foo')
-    .promise();
+  beforeEach(function() {
+    return vm.revert('install').start().promise();
   });
 
-  it('should install dependencies correctly', function() {
-    return vm.run('../scripts/build/build_deps_linux.sh')
-    .run('git version')
-    .run('npm version')
-    //.snapshot('deps')
-    .promise();
+  it('should install git', function() {
+    return vm.run('git version').promise();
   });
 
-  it('should be in dev mode', function() {
-    return vm.run('mkdir /home/kalabox/.kalabox')
-    //.revert('deps')
-    .run('echo "{"devMode": "true"}" > /home/kalabox/.kalabox/kalabox.json')
-    .promise();
+  it('should install npm', function() {
+    return vm.run('npm version').promise();
   });
-
-  it.skip('should install kalabox correctly', function() {
-    return vm.run('../scripts/install/install_linux.sh')
-    .revert('deps')
-    .run('kbox config')
-    .promise();
-  });
-
-  /*it('should install correctly', function() {
-    return ted.driver.vm(tag, {gui: true})
-    .run('which echo')
-    .run('../scripts/build/build_deps_linux.sh')
-    .run('../scripts/install/install_linux.sh')
-    .restart()
-    .run('/home/kalabox/kalabox/bin/kbox.js config')
-    .done();
-  });*/
 
 });
