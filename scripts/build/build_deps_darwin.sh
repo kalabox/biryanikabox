8086f9b7efb3cfd54e33efc5e5a78285ee495656
Mike Pirog <mike@kalamuna.com>
2015-12-11 19:35:29 -0800

    #810: We need a no-ui way to install git on Darwin

--- scripts/build/build_deps_darwin.sh
+++ scripts/build/build_deps_darwin.sh
@@ -16,65 +16,40 @@
 
 # Set some things in the environment for us to use
 SUDO_PASSWORD="kalabox"
+MAJOR_MAC_VERSION=$(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}')
+CLI_MOUNT="Command Line Developer Tools"
 
-# Get TCUTIL to help us manage accessability things
-cd /tmp
-curl -O https://raw.githubusercontent.com/kalabox/tccutil/master/tccutil.py
-echo "${SUDO_PASSWORD}" | sudo -S \
-  mkdir -p /usr/local/bin
-echo "${SUDO_PASSWORD}" | sudo -S \
-  mv tccutil.py /usr/local/bin/tccutil.py
-echo "${SUDO_PASSWORD}" | sudo -S \
-  chmod +x /usr/local/bin/tccutil.py
+# Get the correct meta
+if [ "${MAJOR_MAC_VERSION}" == "10.9" ]; then
+  CLI_BASE_NAME=command_line_tools_for_os_x_mavericks_late_july_2014.dmg
+  CLI_PACKAGE="Command Line Tools (OS X 10.9).pkg"
+elif [ "${MAJOR_MAC_VERSION}" == "10.10" ]; then
+  CLI_BASE_NAME=Command_Line_Tools_OS_X_10.10_for_Xcode_7.2.dmg
+  CLI_PACKAGE="Command Line Tools (OS X 10.10).pkg"
+elif [ "${MAJOR_MAC_VERSION}" == "10.11" ]; then
+  CLI_BASE_NAME=Command_Line_Tools_OS_X_10.11_for_Xcode_7.2.dmg
+  CLI_PACKAGE="Command Line Tools (OS X 10.10).pkg"
+fi
 
-# White list the needed apps
-echo "${SUDO_PASSWORD}" | sudo -S \
-  tccutil.py --insert /usr/bin/osascript
-echo "${SUDO_PASSWORD}" | sudo -S \
-  tccutil.py --insert com.apple.Terminal
-echo "${SUDO_PASSWORD}" | sudo -S \
-  tccutil.py --enable com.apple.Terminal
+# Make sure /usr/local/bin exists
+echo "${SUDO_PASSWORD}" | \
+  sudo -S mkdir -p /usr/local/bin
 
-# Install the command line tools
-xcode-select --install
-sleep 5
-osascript <<EOD
-  tell application "System Events"
-    tell process "Install Command Line Developer Tools"
-      keystroke return
-      click button "Agree" of window "License Agreement"
-    end tell
-  end tell
-EOD
-
-# Wait until we have installed command line tools
-osascript <<EOD
-  tell application "Finder"
-    repeat
-      if exists "/Library/Developer/CommandLineTools/usr/bin/git" as POSIX file then
-        exit repeat
-      else
-        delay 0.5
-      end if
-    end repeat
-  end tell
-EOD
-
-# Small delay to wait for below window to show up
-sleep 10
-
-# Close out the command tools install
-osascript <<EOD
-  tell application "System Events"
-    tell process "Install Command Line Developer Tools"
-      click button "Done" of window 1
-    end tell
-  end tell
-EOD
-
-# Move to tmp and download the node package
+# Move to tmp and download needed packages
+CLI_DOWNLOAD_URL=https://s3-us-west-1.amazonaws.com/kb-testing-assets/deps/${CLI_BASE_NAME}
 cd /tmp
 curl -LO https://nodejs.org/dist/v4.2.2/node-v4.2.2.pkg
+curl -LO $CLI_DOWNLOAD_URL
+
+# Mount the command line tools
+hdiutil attach "/tmp/${CLI_BASE_NAME}"
+
+# Install the CLI Package
+echo "${SUDO_PASSWORD}" | \
+  sudo -S installer -pkg "/Volumes/${CLI_MOUNT}/${CLI_PACKAGE}" -target /
+
+# DISSSSSMOUNT
+hdiutil detach "/Volumes/${CLI_MOUNT}"
 
 # Install the node package
 echo "${SUDO_PASSWORD}" | \
